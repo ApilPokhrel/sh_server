e68e0c911ae2c7cec50c3634c1b591a294442450
asus <apilpokharel5@gmail.com>
2020-07-10 23:22:48 +0545

    config hide

--- config/setup.js
+++ config/setup.js
@@ -5,7 +5,7 @@ let init = async () => {
   let rPayload = {
     name: "super_admin",
     is_admin: true,
-    permissions: [],
+    permissions: [],d
     desc: "Super Admin can do anything"
   };
 
